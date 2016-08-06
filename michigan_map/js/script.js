0f4462e45562e400c238b3a78fe650573ebda970
Stephen Smith <blackrim@gmail.com>
2016-08-05 23:52:17 -0400

    Update script.js

--- michigan_map/js/script.js
+++ michigan_map/js/script.js
@@ -191,7 +191,7 @@ legendDIV.onAdd = function (map) {
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
-            '<i style="background:' + setColorPD(grades[i] + 1) + '"></i> ' +
+            '<i style="background:' + setColorDIV(grades[i] + 1) + '"></i> ' +
             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
 
